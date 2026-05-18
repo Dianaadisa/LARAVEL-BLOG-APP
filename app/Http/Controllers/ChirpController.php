cf101fbb8503e0fd234e46d8d2f039ea13ac885f
Susan Imali <simali@ke.ci.org>
2026-05-18 07:09:31 +0300

    Initial commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Chirp;
+
+class ChirpController extends Controller
+{
+    public function index()
+    {
+        $chirps = Chirp::latest()->get();
+
+        return view('home', [
+            'chirps' => $chirps
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null,
+        ]);
+
+        return redirect('/')->with('success', 'Your chirp has been posted!');
+    }
+}
